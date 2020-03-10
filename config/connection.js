26b47fc89964f8448249c77dcf63c1870b0f1eb6
MinJin Park <parkmin1986@gmail.com>
2020-03-10 18:27:46 -0400

    trying to deloy with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "kproot012",
-  database: "burgers_db"
-});
+let connection;
 
-// Make connection.
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "kproot012",
+    database: "burgers_db"
+  });
+}
+
 connection.connect(err => {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,5 +22,5 @@ connection.connect(err => {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
